6831893428f7da831a1ddcc851882a2d41b3a6dc
Kalyani Chavan <kalyanichavan197@gmail.com>
2026-09-11 15:51:40 +0530

    Update API_URL to production endpoint

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "const API_URL = "https://student-management-2-4scm.onrender.com";
 
 const studentForm = document.getElementById("studentForm");
 const studentTable = document.getElementById("studentTable");
